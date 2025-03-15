b9465bc290c287c9c64e6fe173b0ee7e6467e7bc
Hammad Hassan <hammad.hassan.murtaza@gmail.com>
2025-03-15 12:27:44 +0500

    sw register, install, delete, sw extendable events

--- js/app.js
+++ js/app.js
@@ -8,7 +8,23 @@ const APP = {
                 registeration.waiting ||
                 registeration.active;
                 console.log( "service worker registered" );
-            } )
+            } );
+            // check if page is currently has a service worker.
+            if( navigator.serviceWorker.controller ) {
+                console.log( "we have a service worker installed." );
+            }
+
+            // handler to check if a new service worker is installed and activated
+            navigator.serviceWorker.oncontrollerchange = ( ev ) => {
+                console.log( "new service worker activated." )
+            };
+
+            //unregister
+            // navigator.serviceWorker.getRegistrations().then( registerations => {
+            //     for(let reg of registerations) {
+            //         reg.unregister().then( isUnreg => console.log( isUnreg ) )
+            //     }
+            // } )
         } else {
             console.log( "sws is not supported." );
         }
--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-console.log("sw running");
+self.addEventListener( "install", ( event ) => {
+    console.log( "installed" )
+} );
+
+
+self.addEventListener( "activate", ( event ) => {
+    console.log( "activated" )
+} );
+
+
+self.addEventListener( "fetch", ( event ) => {
+    console.log( "intercept an http request", event.request )
+} );
+
+
+self.addEventListener( "message", ( event ) => {
+    console.log( "message from message" )
+} );
